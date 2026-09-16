3bc33462e9a7c7450fb18f3072c9939e3d9dd033
allanarenasoli <86263937+allanarenasoli@users.noreply.github.com>
2026-09-15 23:03:35 -0300

    Configura email de suporte do EQShift

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* Edite estes valores antes de publicar. Nunca coloque segredos aqui. */
 window.EQSHIFT_SITE = {
-  supportEmail: "SEU_EMAIL_DE_SUPORTE",
+  supportEmail: "eqshift.app@gmail.com",
   playStoreUrl: "#",
   githubUrl: "#"
 };
